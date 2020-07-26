2fd046a248eea42a39214b4840ea5e12f64af571
Callmeqaqq <68635081+Callmeqaqq@users.noreply.github.com>
2020-07-27 03:43:52 +0700

    Add files via upload

---
+++ bai7.c
@@ -0,0 +1,12 @@
+//
+#include<stdio.h>
+#define Pi 3.14159
+int main ()
+{float banKinh, pi;
+printf("nhap ban kinh hinh tron");
+scanf("%f", &banKinh);
+printf("dien tich hinh tron la: %f*%f*%f = %f", banKinh,banKinh, Pi, banKinh*banKinh * Pi);
+printf("\nchu vi hinh tron la: %f*2 *%f=%f", banKinh, Pi, banKinh*2*Pi);
+return 0;
+}
+
