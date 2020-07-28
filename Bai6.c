d63d3f4962e1b0e61ffdd84eeeaa0423a2514cf6
Callmeqaqq <68635081+Callmeqaqq@users.noreply.github.com>
2020-07-29 01:00:06 +0700

    Update Bai6.c

--- Bai6.c
+++ Bai6.c
@@ -1,15 +1,14 @@
 //
 #include<stdio.h>
 
-int main ()
-{float chieuDai, chieuNgan;//De bai khong phai so Nguyen nen dung float cho ca truong hop so thuc
+int main (){
+  float chieuDai, chieuNgan;
 printf("Nhap vao chieu dai:");
 scanf("%f", &chieuDai);
 printf("nhap vao chieu ngan:");
 scanf("%f", &chieuNgan);
 
-printf("chu vi la:%f * %f = %f", chieuDai, chieuNgan, chieuDai * chieuNgan);
-
+  printf("dien tich la:%f * %f = %f", chieuDai, chieuNgan, chieuDai * chieuNgan);
+  printf("\nchu vi la:(%f + %f) *2 = %f", chieuDai, chieuNgan,(chieuDai+chieuNgan)*2);
 return 0;
 }
-
