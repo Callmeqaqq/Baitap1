9a042ab92f0133d43fbd05adb4593cede0e5cfea
namphth1807043 <43373102+namphth1807043@users.noreply.github.com>
2020-07-28 17:05:49 +0700

    Update bai_5_2.c

--- bai_5_2.c
+++ bai_5_2.c
@@ -1,10 +1,10 @@
 //
 #include<stdio.h>
-
+// làm thiếu yêu cầu đề bài, đề bài yêu cầu nhập số vào chứ không phải khai báo số sẵn,
 int main ()
 {
-int a = 5 , b=15 ,c= 16 , d = 7, max =a ;
-max = a ;
+int a = 5 , b=15 ,c= 16 , d = 7, max =a ;// format lại code, space bua bai qua
+max = a ; // dòng này có ý nghĩa gì?
 
 if (b>max){
 	max = b ;
@@ -29,4 +29,5 @@ else{
 }
 return 0;
 }
+// truong hop 4 so bang nhau thi sao ?
 
