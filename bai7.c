e63308b2cf4fa5740ff0101ce44362a43796ccfe
Callmeqaqq <68635081+Callmeqaqq@users.noreply.github.com>
2020-07-29 12:26:22 +0700

    Update bai7.c

--- bai7.c
+++ bai7.c
@@ -1,12 +1,14 @@
 //
 #include<stdio.h>
+#include<math.h>
 #define Pi 3.14159
-int main ()
-{float banKinh, pi;
-printf("nhap ban kinh hinh tron");
+
+int main (){
+float banKinh, pi;
+
+printf("nhap ban kinh hinh tron:");
 scanf("%f", &banKinh);
-printf("dien tich hinh tron la: %f*%f*%f = %f", banKinh,banKinh, Pi, banKinh*banKinh * Pi);
-printf("\nchu vi hinh tron la: %f*2 *%f=%f", banKinh, Pi, banKinh*2*Pi);
+printf("dien tich hinh tron la: (%f^2)*%f=%f", banKinh, Pi, pow(banKinh, 2) * Pi);
+printf("\nchu vi hinh tron la: %f*2*%f=%f", banKinh, Pi, banKinh * 2 * Pi);
 return 0;
 }
-
