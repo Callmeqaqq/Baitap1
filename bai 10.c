31377262935a8f53196c7f2f3ae5914c4dd669ff
Callmeqaqq <68635081+Callmeqaqq@users.noreply.github.com>
2020-08-14 00:59:03 +0700

    Update bai 10.c

--- bai 10.c
+++ bai 10.c
@@ -7,12 +7,16 @@ int main ()
 printf(" nhap lan luot chieu dai cac canh:");
 scanf("%f%f%f", &a, &b, &c);
 
-	if((abs(a - b)) < b < (a + c)){//dung hang dang thuc tam giac
-	printf("3 so hop le de tao thanh 1 tam giac");
-}
+ if ( a > 0 && b > 0 && c > 0){// cho nay ban dau em thu la cho cac so < 0 roi in ket qua vui long nhap so duong luon nhung ma no khong chay, li do tai sao v a?
+		if((abs(a - b)) < b < (a + c)){//dung hang dang thuc tam giac
+			printf("3 so hop le de tao thanh 1 tam giac");
+		}
 		else{
 			printf(" 3 so khong hop le de tao thanh 1 tam giac");
-			}	
+			}
+	}
+	else{ 
+		 printf("Chu y: vui long nhap so duong!");
+	}
 return 0;
 }
-
